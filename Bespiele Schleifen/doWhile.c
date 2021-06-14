7fb060857179116e5212a7b49e9baf26315f57e6
Marvin Haupenthal <76146648+marvinhaup@users.noreply.github.com>
2021-06-14 10:21:10 +0200

    ergänzt

--- Bespiele Schleifen/doWhile.c
+++ Bespiele Schleifen/doWhile.c
@@ -3,6 +3,15 @@
 
 int main() {
 
+    int count;
+    printf("\nAnzahl der Durchlaeufe eingeben: ");
+    scanf("%d", &count);
+
+    int i = 0;
+    do{
+        printf("\nDurchlauf Nummer: %d", i+1);
+        i++;
+    } while(i < count);
 
     return 0;
 }
--- Bespiele Schleifen/for.c
+++ Bespiele Schleifen/for.c
@@ -3,7 +3,33 @@
 
 int main() {
 
+    int count;
+    printf("\nAnzahl der Durchlaeufe eingeben: ");
+    scanf("%d", &count);
+
+    for (int i = 0; i < count; i++)
+    {
+        printf("\nDurchlauf Nummer: %d", i+1);
+    }
     
 
+
+
+
+/*
+    int alter = 0;
+    for(int i = 0; i < 5; i++) {
+        printf("\nGeben Sie ihr alter ein(5-100):");
+        scanf("%d", &alter);
+        if(alter > 4 && alter < 101){
+            break;
+        }
+        else{
+            i--;
+            printf("\nFalsche Eingabe");
+        }
+        printf("\nWert der Laufvariable: %d", i);
+    }
+*/
     return 0;
 }

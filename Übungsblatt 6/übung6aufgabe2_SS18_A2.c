d14bccde7f1bdcd3c59d9d500dab0d54b709b674
Marvin Haupenthal <76146648+marvinhaup@users.noreply.github.com>
2021-06-21 21:26:54 +0200

    übung 6 lösungen fertiggestellt

--- Übungsblatt 6/übung6aufgabe2_SS18_A2.c
+++ Übungsblatt 6/übung6aufgabe2_SS18_A2.c
@@ -1,8 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int bestimmeLaenge(char[]);
 
-
+int main()
+{
+    char text[30] = "Umwelt-Campus Birkenfeld";
+    int laenge = bestimmeLaenge(text);
+    printf("Die Laenge ist: %d", laenge);
     return 0;
+}
+
+int bestimmeLaenge(char arr[]){
+    int i = 0;
+    int zaehler = 0;
+    while(arr[i] != '\0'){
+        zaehler++;
+        i++;
+    }
+    return zaehler;
 }
--- Übungsblatt 6/übung6aufgabe3_WS18_A2.c
+++ Übungsblatt 6/übung6aufgabe3_WS18_A2.c
@@ -1,8 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int anzahlWoerter(char[]);
+
 int main() {
 
+    int anzahl = 0;
+    char meineWoerter[] = "Das ist ein Test!";
+    anzahl = anzahlWoerter(meineWoerter);
+    printf("\nAnzahl der woerter: %d", anzahl);
 
     return 0;
+}
+
+int anzahlWoerter(char uebergabeWoerter[]){
+    int anzahl = 1;
+    int i = 0;
+    while (uebergabeWoerter[i] != '\0')
+    {
+        if(uebergabeWoerter[i] == ' '){
+            anzahl++;
+        }
+        i++;
+    }
+    return anzahl;
 }
